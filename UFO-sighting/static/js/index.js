b4ab9cb6b34d0e845a02d4a93c3ac3c3824836f9
unknown <ryanklueg@gmail.com>
2020-05-02 23:06:42 -0400

    added table condensed shape filter code

--- UFO-sighting/static/js/index.js
+++ UFO-sighting/static/js/index.js
@@ -1,14 +1,24 @@
 //Create the dropdown menu for shape filter
-
 //Create list of unique shapes
 const shapes = data.map(ufo => ufo.shape);
 const uniqueShapes = Array.from(new Set(shapes));
-
 //Create filter option for each shape
 //Set select tag to append options too
 const shapeFilter = d3.select("#shape");
 //Loop through list of unique shapes to append each as an option
-uniqueShapes.forEach(shape => {
-    let option = shapeFilter.append("option");
-    option.text(shape);
+uniqueShapes.forEach(shape => shapeFilter.append("option").text(shape));
+
+//Create table from data
+//Select table body
+const tbody = d3.select("tbody");
+//Iterate through data to append to table
+data.forEach(ufo => {
+    let row = tbody.append("tr");
+    row.append("td").text(ufo.datetime);
+    row.append("td").text(ufo.city);
+    row.append("td").text(ufo.state);
+    row.append("td").text(ufo.country);
+    row.append("td").text(ufo.shape);
+    row.append("td").text(ufo.durationMinutes);
+    row.append("td").text(ufo.comments);
 });
