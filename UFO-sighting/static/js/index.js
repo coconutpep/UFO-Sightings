a4070ee02a3b99191614829895b62a78ddb18d4b
unknown <ryanklueg@gmail.com>
2020-05-07 18:06:43 -0400

    Added date filter for table

--- UFO-sighting/static/js/index.js
+++ UFO-sighting/static/js/index.js
@@ -1,24 +1,69 @@
-//Create the dropdown menu for shape filter
-//Create list of unique shapes
-const shapes = data.map(ufo => ufo.shape);
-const uniqueShapes = Array.from(new Set(shapes));
-//Create filter option for each shape
-//Set select tag to append options too
-const shapeFilter = d3.select("#shape");
-//Loop through list of unique shapes to append each as an option
-uniqueShapes.forEach(shape => shapeFilter.append("option").text(shape));
-
+//Read in data as a different variable for changing
+let ufos = data;
+//Select inputs for later filtering
+const dateInput = d3.select("#datetime");
+const form = d3.select("form");
 //Create table from data
+//select table
+const table = d3.select("table");
 //Select table body
 const tbody = d3.select("tbody");
-//Iterate through data to append to table
-data.forEach(ufo => {
-    let row = tbody.append("tr");
-    row.append("td").text(ufo.datetime);
-    row.append("td").text(ufo.city);
-    row.append("td").text(ufo.state);
-    row.append("td").text(ufo.country);
-    row.append("td").text(ufo.shape);
-    row.append("td").text(ufo.durationMinutes);
-    row.append("td").text(ufo.comments);
-});
+//Create Function to title case cities
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+//Function to filter data
+function runFilter() {
+    //Prevent page refresh
+    d3.event.preventDefault();
+    //Create iterable array of table rows
+    const rows = table.selectAll("tr")["_groups"][0];
+    //Grab the inputs
+    const inputDate = dateInput.property("value");
+    //Conditional Statement to filter table based on input
+    //Date was inputted
+    if (typeof inputDate === "string") {
+        //For loop iterates through rows and hides data based on input
+        for (let i=1; i<rows.length; i++) {
+            //Grabs date from row
+            const dataDate = rows[i]["cells"][0]["textContent"];
+            //Conditional statement to determine if row's date matches input
+            if (dataDate === inputDate) {
+                //Show data that matches
+                rows[i].style.display="";
+            }
+            else {
+                //Hide data that doesn't match
+                rows[i].style.display="none";
+            }
+        }
+    }
+}
+
+//Create table
+function createTable (arr){
+    arr.forEach(ufo => {
+        let row = tbody.append("tr");
+        row.append("td").text(ufo.datetime);
+        //Append Title Cased cities
+        row.append("td").text(toTitleCase(ufo.city));
+        //Append Capatalized states
+        row.append("td").text(ufo.state.toUpperCase());
+        //Append Capatilized Country Codes
+        row.append("td").text(ufo.country.toUpperCase());
+        row.append("td").text(ufo.shape);
+        row.append("td").text(ufo.durationMinutes);
+        row.append("td").text(ufo.comments);
+    });
+}
+createTable(ufos);
+
+//Filter data based on form submission
+dateInput.on("change", runFilter);
+form.on("submit", runFilter);
